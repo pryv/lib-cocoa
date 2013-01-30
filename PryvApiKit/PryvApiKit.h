a29d3ab4b6c1c1b92f1e08ef21f65fa1de13871e
Konstantin Dorodov <konstantin@dorodov.com>
2013-01-30 14:54:21 +0100

    initial commit

---
+++ PryvApiKit/PryvApiKit.h
@@ -0,0 +1,13 @@
+//
+//  PryvApiKit.h
+//  PryvApiKit
+//
+//  Created by Konstantin Dorodov on 1/30/13.
+//  Copyright (c) 2013 Pryv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PryvApiKit : NSObject
+
+@end
