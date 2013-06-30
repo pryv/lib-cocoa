bbc8760f682c2852c3e0d25a3ea087dadfea2785
Nenad Jelic <jelic.nenad@gmail.com>
2013-06-30 15:59:34 +0200

    project changes

--- iOs Example/PYEViewController.m
+++ iOs Example/PYEViewController.m
@@ -25,11 +25,7 @@
     [PYClient setDefaultDomainStaging];
     
     PYAccess *access = [PYClient createAccessWithUsername:@"perkikiki" andAccessToken:kPYUserTempToken];
-    NSLog(@"isOnline %d",access.isOnline);
-    NSLog(@"log");
-    
-//    PYEvent *event = [PYEventsCachingUtillity getEventFromCacheWithEventId:@"eT3iGs4W05"];
-    
+        
     [access getAllChannelsWithRequestType:PYRequestTypeSync
                         gotCachedChannels:^(NSArray *cachedChannelList) {
         NSLog(@"cachedChannelList %@",cachedChannelList);
@@ -38,33 +34,6 @@
             //Nenad_test channel
             if ([channel.channelId isEqualToString:@"TVKoK036of"]) {
                 
-//                [channel getAllEventsWithRequestType:PYRequestTypeSync gotCachedEvents:^(NSArray *cachedEventList) {
-//                    
-//                } gotOnlineEvents:^(NSArray *onlineEventList) {
-//                    
-//                } successHandler:^(NSArray *eventsToAdd, NSArray *eventsToRemove, NSArray *eventModified) {
-//                    
-//                } errorHandler:^(NSError *error) {
-//                    
-//                }];
-
-//                PYEvent *event = [[PYEvent alloc] init];
-//                event.value = @"attachment value1";
-//                event.eventFormat = @"txt";
-//                event.eventClass = @"note";
-//                NSString *imageDataPath = [[NSBundle mainBundle] pathForResource:@"Default" ofType:@"png"];
-//                NSData *imageData = [NSData dataWithContentsOfFile:imageDataPath];
-//                PYAttachment *att = [[PYAttachment alloc] initWithFileData:imageData name:@"Default123" fileName:@"SomeFileName123"];
-//                [event addAttachment:att];
-//                
-//                
-//                [channel createEvent:event
-//                         requestType:PYRequestTypeSync
-//                      successHandler:^(NSString *newEventId, NSString *stoppedId) {
-//                    
-//                } errorHandler:^(NSError *error) {
-//                    
-//                }];
             }
         }
     
@@ -78,34 +47,6 @@
             //Nenad_test channel
             if ([channel.channelId isEqualToString:@"TVKoK036of"]) {
                 
-//                PYEvent *event = [[PYEvent alloc] init];
-//                event.value = @"attachment value1";
-//                event.eventFormat = @"txt";
-//                event.eventClass = @"note";
-//                NSString *imageDataPath = [[NSBundle mainBundle] pathForResource:@"Default" ofType:@"png"];
-//                NSData *imageData = [NSData dataWithContentsOfFile:imageDataPath];
-//                PYAttachment *att = [[PYAttachment alloc] initWithFileData:imageData name:@"Default" fileName:@"SomeFileName"];
-//                [event addAttachment:att];
-                
-                
-//                [channel createEvent:event
-//                         requestType:PYRequestTypeSync
-//                      successHandler:^(NSString *newEventId, NSString *stoppedId) {
-//                          
-//                      } errorHandler:^(NSError *error) {
-//                          
-//                      }];
-
-//                [channel getAllEventsWithRequestType:PYRequestTypeSync gotCachedEvents:^(NSArray *cachedEventList) {
-//                    
-//                } gotOnlineEvents:^(NSArray *onlineEventList) {
-//                    
-//                } successHandler:^(NSArray *eventsToAdd, NSArray *eventsToRemove, NSArray *eventModified) {
-//                    
-//                } errorHandler:^(NSError *error) {
-//                    
-//                }];
-
             }
         }
         
