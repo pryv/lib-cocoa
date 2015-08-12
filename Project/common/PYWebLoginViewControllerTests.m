9b8c3f46fb9e732c4eec5f19dba4b5a3d04ac4d9
Perki <pm@perki.com>
2015-08-12 16:39:02 +0200

    Introducing tests for signing window

---
+++ Project/common/PYWebLoginViewControllerTests.m
@@ -0,0 +1,81 @@
+//
+//  PYWebLoginViewControllerTests.m
+//  PryvApiKit
+//
+//  Copyright (c) 2013 Pryv. All rights reserved.
+//
+
+#import "PYBaseConnectionTests.h"
+
+#import "PYConnection.h"
+#import "PYTestsUtils.h"
+#import "PYTestConstants.h"
+
+#import <WebKit/WebKit.h>
+#import <Cocoa/Cocoa.h>
+
+@interface PYWebLoginViewControllerTests : PYBaseConnectionTests <PYWebLoginDelegate>
+@end
+
+@implementation PYWebLoginViewControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+}
+
+- (void)tearDown
+{
+    // insert teardown here
+    
+    [super tearDown];
+}
+
+
+- (void)testWebLogin
+{
+    NSLog(@"Signin Started");
+
+    WebView *webView = [[WebView alloc] init];
+    
+    NSArray *keys = [NSArray arrayWithObjects:  kPYAPIConnectionRequestStreamId,
+                     kPYAPIConnectionRequestLevel,
+                     nil];
+    
+    NSArray *objects = [NSArray arrayWithObjects:   kPYAPIConnectionRequestAllStreams,
+                        kPYAPIConnectionRequestManageLevel,
+                        nil];
+    
+    
+    NSArray *permissions = [NSArray arrayWithObject:[NSDictionary dictionaryWithObjects:objects
+                                                                                forKeys:keys]];
+    
+    //[PYClient setDefaultDomainStaging];
+    __unused
+    PYWebLoginViewController *webLoginController =
+        [PYWebLoginViewController requestConnectionWithAppId:@"pryv-sdk-macosx-tests"
+                                              andPermissions:permissions
+                                                    delegate:self
+                                                 withWebView:&webView];
+    
+    
+}
+
+
+- (void) pyWebLoginSuccess:(PYConnection*)pyConnection {
+    
+    NSLog(@"Signin With Success %@ %@",pyConnection.userID,pyConnection.accessToken);
+    
+}
+
+- (void) pyWebLoginAborted:(NSString*)reason {
+    NSLog(@"Signin Aborted: %@",reason);
+}
+
+- (void) pyWebLoginError:(NSError*)error {
+    NSLog(@"Signin Error: %@",error);
+}
+
+@end
+
